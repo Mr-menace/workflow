a8258799f47b2cc62d48fcab72e862918f63740f
Pips <dennisprotalon@gmail.com>
2020-03-22 03:46:49 +0800

    Functions pa Kulang

---
+++ JQueryBoot/Jindex.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Authentication Site</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm bg-info navbar-light">
+  <!-- Brand/logo -->
+  <a class="navbar-brand" href="#">
+    <img src="img/dbz.jpg" class="rounded-circle"  alt="logo" style="width:50px;">
+  </a>
+  
+  <!-- Links -->
+  <ul class="navbar-nav">
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Dragon Ball Series</a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">Dragon Ball</a>
+        <a class="dropdown-item" href="#">Dragon Ball Z</a>
+        <a class="dropdown-item" href="#">Dragon Ball Z Kai</a>
+        <a class="dropdown-item" href="#">Dragon Ball Super</a>
+        <a class="dropdown-item" href="#">Dragon Ball Super</a>
+      </div>
+    
+    </li>
+    <li class="nav-item">
+      <a class="nav-link " href="#">Gallery</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Stories</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Videos</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Dragon Ball Games</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Contact Us</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Login</a>
+    </li>
+
+    <li class="na-item">
+    <nav class="navbar navbar-expand-sm  navbar-dark">
+  <form class="form-inline" action="Jindex.php">
+    <input class="form-control mr-sm-2" type="text" placeholder="Search">
+    <button class="btn btn-danger" type="submit">Search</button>
+  </form>
+</nav>
+    </li>
+  </ul>
+</nav>
+
+
+
+<br>
+<div class="row">
+<!--- UMPISA NG ROW CLASS -->
+
+  <div class="col-xl-4">
+    <div class="card bg-success text-white" style="width:100%"  >
+    <img class="card-img-top" src="img/gohan.gif" alt="Card image">
+      <div class="card-body">
+        <h5 class="card-title">SSJ2 Gohan</h5>
+        <p class="card-text">While Fighting Cell Gohan Unleashed Incredible Power Due to Anger.</p>
+        <a href="#" class="btn btn-primary">Story of Gohan</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-xl-4">
+    <div class="card bg-warning text-white"  style="height:42.50rem;">
+    <img class="card-img-top" src="img/Jiren.jpg" alt="Card image " style="width:100%">
+      <div class="card-body">
+        <h5 class="card-title">Jiren</h5>
+        <p class="card-text">A Character that Stronger than their Universe God Itself.</p>
+        <a href="#" class="btn btn-danger">Story of Jiren</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-xl-4">
+    <div class="card bg-danger text-white"  style="height:42.50rem;">
+    <img class="card-img-top" src="img/goku_ui.jpg" alt="Card image" style="width:100%">
+      <div class="card-body">
+        <h5 class="card-title">Goku</h5>
+        <p class="card-text">Father of Gohan Who loves to fight strong foes that Excites him.</p>
+        <a href="#" class="btn btn-warning">Story of Goku</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-xl-4">
+  <br>
+    <div class="card bg-danger text-white" style="height:79.50rem;" >
+    <img class="card-img-top" src="img/vegeta.jpg" alt="Card image" style="width:100%">
+      <div class="card-body">
+        <h5 class="card-title">Vegeta</h5>
+        <p class="card-text">The Saiyan Prince and Pride, Also the Father of Trunks.</p>
+        <a href="#" class="btn btn-info">Story of Vegeta</a>
+      </div>
+<!-- ====================================================================================  -->
+      <img class="card-img-top" src="img/beerus.png" alt="Card image" style="width:100%">
+      <div class="card-body">
+        <h5 class="card-title">Lord Beerus</h5>
+        <p class="card-text">The God of Destruction of Universe 7.</p>
+        <a href="#" class="btn btn-info">Story of Beerus</a>
+      </div>
+<!-- ====================================================================================  -->
+
+    </div>
+  </div>
+
+  <div class="col-xl-4">
+  <br>
+    <div class="card bg-info text-white"  style="height:79.50rem;">
+    <img class="card-img-top" src="img/trunks.png" alt="Card image" style="width:100%">
+      <div class="card-body">
+        <h5 class="card-title">Future Trunks</h5>
+        <p class="card-text">Son of Vegeta and Bulma, Also a half blood Saiyan and Human.</p>
+        <a href="#" class="btn btn-danger">Story of Trunks</a>  
+            
+</div>
+<!-- ====================================================================================  -->
+<img class="card-img-bottom" src="img/goten.jpg" alt="Card image" style="width:100%">
+<div class="card-body">
+        <h5 class="card-title">Goten</h5>
+        <p class="card-text">Son of Kakarot or Goku also a Super Saiyan at a Young Age.</p>
+        <a href="#" class="btn btn-danger">Story of Goten</a>  
+        </div>
+<!-- ====================================================================================  -->
+</div>
+</div>
+
+<div class="col-xl-4">
+  <br>
+    <div class="card bg-danger text-white" >
+    <img class="card-img-top" src="img/frieza.jfif" alt="Card image" style="width:100%">
+      <div class="card-body">
+        <h5 class="card-title">Frieza</h5>
+        <p class="card-text">The Emperor of Evil Loves to Conquer Planets and Destroying
+        He is also the Mortal Enemy of Goku and Vegeta.</p>
+        <a href="#" class="btn btn-info">Story of Frieza</a>
+      </div>
+
+      <img class="h-50 d-inline-block card-img-top" src="img/whiz.png" alt="Card image" style="width:100%" >
+      <div class="card-body">
+        <h5 class="card-title">Whiz</h5>
+        <p class="card-text">The Emperor of Evil Loves to Conquer Planets and Destroying
+        He is also the Mortal Enemy of Goku and Vegeta.</p>
+        <a href="#" class="btn btn-info">Story of Whiz</a>
+      </div>
+
+    </div>
+  </div>
+
+
+</div><!-- CLOSING FOR ROW CLASS-->
+
+    
+</body>
+</html>
