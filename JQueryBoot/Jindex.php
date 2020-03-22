eba484811bddd92feb6ead30b038274260f58a9f
Pips <dennisprotalon@gmail.com>
2020-03-23 04:08:08 +0800

    Updated Dropdown Contents and Social media Links etc..

--- JQueryBoot/Jindex.php
+++ JQueryBoot/Jindex.php
@@ -3,73 +3,118 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Authentication Site</title>
+    <title>Home</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+  <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+
+  <style>
+/* para sa outline natin*/
+.dotted {outline-style: groove; outline-color:red;}
+
+</style>
+
 </head>
 <body>
 
-<nav class="navbar navbar-expand-sm bg-info navbar-light">
+<nav class="navbar navbar-expand-sm bg-light navbar-warning">
   <!-- Brand/logo -->
   <a class="navbar-brand" href="#">
     <img src="img/dbz.jpg" class="rounded-circle"  alt="logo" style="width:50px;">
   </a>
   
   <!-- Links -->
+  
   <ul class="navbar-nav">
     <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Dragon Ball Series</a>
+ <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown"><i class='fas fa-film' style='font-size:15px'></i>&nbsp;Dragon Ball Series</a>
       <div class="dropdown-menu">
-        <a class="dropdown-item" href="#">Dragon Ball</a>
-        <a class="dropdown-item" href="#">Dragon Ball Z</a>
-        <a class="dropdown-item" href="#">Dragon Ball Z Kai</a>
-        <a class="dropdown-item" href="#">Dragon Ball Super</a>
-        <a class="dropdown-item" href="#">Dragon Ball Super</a>
-      </div>
-    
+      <h5 class="dropdown-header"><img src="img/db_series.jpg" class="img-responsive"  alt="logo" style="width:30px;"><font color="#C1831E">&nbsp;Dragonball Series</font></h5>
+        <a class="dropdown-item" href="#"><img src="img/db.jpg" class="img-responsive"  alt="logo" style="width:50px;">&nbsp;Dragon Ball</a>
+        <a class="dropdown-item" href="#"><img src="img/dbz_ser.jpg" class="img-responsive"  alt="logo" style="width:50px;">&nbsp;Dragon Ball Z</a>
+        <a class="dropdown-item" href="#"><img src="img/dbz_kai.jpg" class="img-responsive"  alt="logo" style="width:50px;">&nbsp;Dragon Ball Z Kai</a>
+        <a class="dropdown-item" href="#"><img src="img/dbsuper.jpg" class="img-responsive"  alt="logo" style="width:50px;">&nbsp;Dragon Ball Super</a>
+      
+      <!---Movies-->
+        <h5 class="dropdown-header"><img src="img/db_movies.jpg" class="img-responsive"  alt="logo" style="width:30px;"><font color="#C1831E">&nbsp;Dragonball Movies</font></h5>
+        <a class="dropdown-item" href="#"><img src="img/dbmovies/db_curse.jpg" class="img-responsive"  alt="logo" style="width:50px;">&nbsp;Dragonball: Curse of the Blood Rubies</a>
+        <a class="dropdown-item" href="#"><img src="img/dbmovies/db_princess.jpg" class="img-responsive"  alt="logo" style="width:50px;">&nbsp;Dragonball: Sleeping Princess in Devil Castle</a>
+        <a class="dropdown-item" href="#"><img src="img/dbmovies/db_path_to_power.jpg" class="img-responsive"  alt="logo" style="width:50px;">&nbsp;Dragonball: The Path to Power</a>
+        <a class="dropdown-item" href="#"><img src="img/dbmovies/db_mystical.jpg" class="img-responsive"  alt="logo" style="width:50px;">&nbsp;Dragonball: Mystical Adventure</a>
+        <a class="dropdown-item" href="#"><img src="img/dbmovies/dbz_deadzone.png" class="img-responsive"  alt="logo" style="width:50px;">&nbsp;Dragonball Z: Dead Zone</a>
+        <a class="dropdown-item" href="#"><img src="img/dbmovies/dbz_strongest.jpg" class="img-responsive"  alt="logo" style="width:50px;">&nbsp;Dragonball Z: the World's Strongest</a>
+        <a class="dropdown-item" href="#"><img src="img/dbmovies/dbz_tree.jpg" class="img-responsive"  alt="logo" style="width:50px;">&nbsp;Dragonball Z: the Tree of Might</a>
+        <a class="dropdown-item" href="#"><img src="img/dbmovies/dbz_slug.jpg" class="img-responsive"  alt="logo" style="width:50px;">&nbsp;Dragonball Z: Lord Slug</a>
+        <a class="dropdown-item" href="#"><img src="img/dbmovies/dbz_cooler.png" class="img-responsive"  alt="logo" style="width:50px;">&nbsp;Dragonball Z: Cooler's Revenge</a>
+        <a class="dropdown-item" href="#"><img src="img/dbmovies/dbz_metacooler.png" class="img-responsive"  alt="logo" style="width:50px;">&nbsp;Dragonball Z: The Return of Cooler</a>
+        <a class="dropdown-item" href="#"><img src="img/dbmovies/dbz_broly1.jpg" class="img-responsive"  alt="logo" style="width:50px;">&nbsp;Dragonball Z: Broly The Legendary Super Saiyan</a>
+        <a class="dropdown-item" href="#"><img src="img/dbmovies/dbz_bojack.jpg" class="img-responsive"  alt="logo" style="width:50px;">&nbsp;Dragonball Z: Bojack Unbound</a>
+        <a class="dropdown-item" href="#"><img src="img/dbmovies/dbz_broly2.jpg" class="img-responsive"  alt="logo" style="width:50px;">&nbsp;Dragonball Z: Broly Second Coming</a>
+        <a class="dropdown-item" href="#"><img src="img/dbmovies/dbz_broly3.jpg" class="img-responsive"  alt="logo" style="width:50px;">&nbsp;Dragonball Z: Bio-Broly</a>
+        <a class="dropdown-item" href="#"><img src="img/dbmovies/dbz_fusion.jpg" class="img-responsive"  alt="logo" style="width:50px;">&nbsp;Dragonball Z: Fusion Reborn</a>
+        <a class="dropdown-item" href="#"><img src="img/dbmovies/dbz_wrath.jpg" class="img-responsive"  alt="logo" style="width:50px;">&nbsp;Dragonball Z: Wrath of the Dragon</a>
+        <a class="dropdown-item" href="#"><img src="img/dbmovies/dbz_battle_of_gods.jpg" class="img-responsive"  alt="logo" style="width:50px;">&nbsp;Dragonball Z: Batlle of Gods</a>
+        <a class="dropdown-item" href="#"><img src="img/dbmovies/dbz_resurrection_of_f.jpg" class="img-responsive"  alt="logo" style="width:50px;">&nbsp;Dragonball Z: Resurrection of F</a>
+        <a class="dropdown-item" href="#"><img src="img/dbmovies/dbs_broly.jpg" class="img-responsive"  alt="logo" style="width:50px;">&nbsp;Dragonball Super: Broly</a>
+        
+  
+    </div>
     </li>
     <li class="nav-item">
-      <a class="nav-link " href="#">Gallery</a>
+      <a class="nav-link " href="#"><i class='fas fa-camera' style='font-size:15px'></i>&nbsp;Gallery</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#">Stories</a>
+      <a class="nav-link" href="#"><i class='fas fa-pencil-alt' style='font-size:15px'></i>&nbsp;Stories</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#">Videos</a>
+      <a class="nav-link" href="#"><i class='fas fa-video' style='font-size:15px'></i>&nbsp;Videos</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#">Dragon Ball Games</a>
+      <a class="nav-link" href="#"><i class='fas fa-dice' style='font-size:15px'></i>&nbsp;Dragon Ball Games</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#">Contact Us</a>
+      <a class="nav-link" href="#"><i class='far fa-comment-alt' style='font-size:15px'></i>&nbsp;Contact Us</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#">Login</a>
+      <a class="nav-link" href="#"><i class='far fa-user-circle' style='font-size:15px'></i>&nbsp;Login</a>
     </li>
 
     <li class="na-item">
     <nav class="navbar navbar-expand-sm  navbar-dark">
   <form class="form-inline" action="Jindex.php">
-    <input class="form-control mr-sm-2" type="text" placeholder="Search">
-    <button class="btn btn-danger" type="submit">Search</button>
+    &nbsp;&nbsp;&nbsp;<input class="form-control mr-sm-2" type="text" placeholder="Search">
+    <button class="btn btn-danger" type="submit">Search&nbsp;<i class='far fa-folder-open' style='font-size:15px'></i></button>
   </form>
 </nav>
     </li>
   </ul>
 </nav>
+<center>
+<h5>Visit Us Thru This Links</h5><nr>
 
+<i class='fab fa-facebook-square' style='font-size:30px;color:#3B5998'></i>&nbsp;
+<i class='fab fa-twitter-square' style='font-size:30px;color:#55ACEE'></i>&nbsp;
+<i class='fab fa-google' style='font-size:30px;color:#dd4b39'></i>&nbsp;
+<i class='fab fa-youtube' style='font-size:30px;color:#bb0000'></i>&nbsp;
+<i class='fab fa-yahoo' style='font-size:30px;color:#430297'></i>&nbsp;
+</center>
 
-
+<h3><font  face="Papyrus" color="#C1831E"><b>&nbsp;&nbsp;Characters</b></h3></font>
+<!-- eto yung sa Style-->
+<div class="dotted">
+<!-- ================ -->
 <br>
-<div class="row">
+
 <!--- UMPISA NG ROW CLASS -->
+<div class="row">
 
   <div class="col-xl-4">
-    <div class="card bg-success text-white" style="width:100%"  >
-    <img class="card-img-top" src="img/gohan.gif" alt="Card image">
+<div class="card bg-success text-white" style="width:100%"  >
+<img class="card-img-top" src="img/gohan.gif" alt="Card image">
       <div class="card-body">
         <h5 class="card-title">SSJ2 Gohan</h5>
         <p class="card-text">While Fighting Cell Gohan Unleashed Incredible Power Due to Anger.</p>
@@ -166,6 +211,11 @@
 
 
 </div><!-- CLOSING FOR ROW CLASS-->
+
+<div class="footer">
+  <p align="center">All rights Reserved to the Owner</p>
+</div>
+
 
     
 </body>
